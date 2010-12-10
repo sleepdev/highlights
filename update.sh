cc89dab1b6a5798ccc9f3c4cbaeb38d2e3643b5d
Andrew <sleepdev@gmail.com>
2010-12-10 00:38:50 -0700

    Added gedit syntax highlighting for sml.

--- update.sh
+++ update.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 
 #also requires appropriate entries in /etc/nanorc to include these files
-cp *.nanorc /usr/share/nano
+cp nanorc/*.nanorc /usr/share/nano
+cp gedit/*.lang /usr/share/gtksourceview-2.0/language-specs
